ec3b6ca3aa6e41630193ebc64783e8e84b2820a2
Ian Macleish <Ian.Macleish@akqa.com>
2016-07-18 17:41:10 -0700

    re-orged project to work with gulp. Added gulp fileinclude (to combine html partials), browsersync (to inject updates into browser), and built the base template for the main page.

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,29 @@
 var gulp        = require('gulp');
+var fileinclude = require('gulp-file-include');
 var browserSync = require('browser-sync');
 var sass        = require('gulp-sass');
+
 var reload      = browserSync.reload;
 
 var src = {
-    scss: 'public/scss/*.scss',
-    css:  'public/css',
-    html: 'public/*.html'
+    scss:       'public/scss/*.scss',
+    css:        'public/css',
+    html:       'public/html/partials/*.html',
+    htmlsrc:    'public/html/src/*.html',
+    indexHTML:  'public/index.html'
 };
 
 // Static Server + watching scss/html files
-gulp.task('serve', ['sass'], function() {
+gulp.task('serve', ['sass','fileinclude'], function() {
 
     browserSync({
         server: "./public"
     });
 
     gulp.watch(src.scss, ['sass']);
-    gulp.watch(src.html).on('change', reload);
+    gulp.watch(src.html, ['fileinclude']);
+    gulp.watch(src.htmlsrc, ['fileinclude']);
+    gulp.watch(src.indexHTML).on('change', reload);
 });
 
 // Compile sass into CSS
@@ -26,6 +32,15 @@ gulp.task('sass', function() {
         .pipe(sass())
         .pipe(gulp.dest(src.css))
         .pipe(reload({stream: true}));
+});
+
+gulp.task('fileinclude', function() {
+  gulp.src(src.htmlsrc)
+    .pipe(fileinclude({
+      prefix: '@@',
+      basepath: '@file'
+    }))
+    .pipe(gulp.dest('./public/'));
 });
 
 gulp.task('default', ['serve']);
